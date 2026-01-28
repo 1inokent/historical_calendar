6a9db7e3bdc8af3db05df3a6721763802a83d40f
Karen <karen@MacBook-Pro-Karen.local>
2026-01-28 18:10:47 +0300

    feat: add title and base tyles

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -3,11 +3,15 @@ import styles from './main-page.module.scss';
 export function MainPage() {
   return (
     <div className={styles.container}>
-      <div>
-        <h1>
-          Исторические <br />
-          даты
-        </h1>
+      <div className={styles.content}>
+        <div className={styles.header}>
+          <img className={styles.icon} src="/svg/Vector.svg" alt="logo" />
+
+          <h1 className={styles.title}>
+            Исторические <br />
+            даты
+          </h1>
+        </div>
       </div>
     </div>
   );
--- webpack.config.js
+++ webpack.config.js
@@ -1,66 +1,22 @@
-// const path = require('path');
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-// module.exports = {
-//   mode: 'development',
-
-//   entry: path.resolve(__dirname, 'src/main.tsx'),
-
-//   output: {
-//     path: path.resolve(__dirname, 'dist'),
-//     filename: 'bundle.[contenthash].js',
-//     clean: true,
-//   },
-
-//   resolve: {
-//     extensions: ['.ts', '.tsx', '.js'],
-//   },
-
-//   module: {
-//     rules: [
-//       {
-//         test: /\.(ts|tsx)$/,
-//         use: 'ts-loader',
-//         exclude: /node_modules/,
-//       },
-//       {
-//         test: /\.css$/,
-//         use: ['style-loader', 'css-loader'],
-//       },
-//     ],
-//   },
-
-//   plugins: [
-//     new HtmlWebpackPlugin({
-//       template: 'public/index.html',
-//     }),
-//   ],
-
-//   devServer: {
-//     port: 3000,
-//     open: true,
-//     hot: true,
-//     historyApiFallback: true,
-//   },
-// };
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const isDev = process.env.NODE_ENV !== 'production';
-
 module.exports = {
-  mode: isDev ? 'development' : 'production',
+  mode: 'development',
 
   entry: path.resolve(__dirname, 'src/main.tsx'),
 
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: isDev ? 'bundle.js' : 'bundle.[contenthash].js',
+    filename: 'bundle.js',
     clean: true,
   },
 
   resolve: {
     extensions: ['.ts', '.tsx', '.js'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
   },
 
   module: {
@@ -70,6 +26,7 @@ module.exports = {
         use: 'ts-loader',
         exclude: /node_modules/,
       },
+
       {
         test: /\.module\.scss$/,
         use: [
@@ -78,21 +35,18 @@ module.exports = {
             loader: 'css-loader',
             options: {
               modules: {
-                localIdentName: isDev ? '[path][name]__[local]' : '[hash:base64:8]',
+                localIdentName: '[name]__[local]__[hash:base64:5]',
               },
             },
           },
           'sass-loader',
         ],
       },
+
       {
         test: /\.scss$/,
         exclude: /\.module\.scss$/,
         use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
       },
     ],
   },
